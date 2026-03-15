40b0c0de229221e88d6d6ed0d6d093a23c94dbfa
najcoding <enoughnaj@gmail.com>
2026-03-15 13:38:16 +0000

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,6 @@
+import array as arr
+array_num = arr.array('i', [1, 3, 5, 3, 7, 9, 3])
+print("Original array: "+str(array_num))
+print("Number of occurences of the number 3 in the said array"+str(array_num.count(3)))
+array_num.reverse()
+print(str(array_num))
